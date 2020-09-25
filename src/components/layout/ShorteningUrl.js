fb0452871a6d4d3a80092944949a1aa5ffda2218
setyobagus01 <setyobagus01@gmail.com>
2020-09-25 14:45:13 +0700

    implement copy text to clipboard

--- src/components/layout/ShorteningUrl.js
+++ src/components/layout/ShorteningUrl.js
@@ -5,29 +5,49 @@ import { ShortenContext } from "../../ShortenContext";
 
 function ShorteningUrl() {
   const [url, setUrl] = useState("");
+  const [error, setError] = useState(null);
   const createLinks = useContext(ShortenContext);
+
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
 
   function handleShortening(e) {
     e.preventDefault();
-    createLinks(url);
+
+    if (url.match(pattern)) {
+      createLinks(url);
+      setError("");
+    } else {
+      setError("Please add a link");
+    }
+
     console.log(url);
   }
 
   return (
-    <>
-      <div className="shorten-container">
-        <ShortenMobileIcon className="bg-shorten" />
-        <form onSubmit={handleShortening}>
-          <input
-            type="text"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            placeholder="Shorten a link here..."
-          />
-          <button id="shorten">Shorten it!</button>
-        </form>
-      </div>
-    </>
+    <div
+      className="shorten-container"
+      style={{ height: error ? "30vh" : "25vh" }}
+    >
+      <ShortenMobileIcon className="bg-shorten" />
+      <form autoComplete="on" onSubmit={handleShortening}>
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          placeholder="Shorten a link here..."
+        />
+        {error ? <span className="error">{error}</span> : null}
+        <button id="shorten">Shorten it!</button>
+      </form>
+    </div>
   );
 }
 
